9600c4221ecf44309e01de16a9feb4de652fbab3
gundamdouble00 <73976668+gundamdouble00@users.noreply.github.com>
2025-01-15 09:38:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { UserProfile } from "./One/UserProfile";
+
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-red-50">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
